633973dbce05a31adaca2b2e2bb89d7bd1ec1bb9
Ebla Ekerdiker <eblaekerdiker@gmail.com>
2023-05-07 19:56:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const drivers = ['Ali', 'Veli', 'Mehmet', 'Hakan'];
+
+const returnFirstTwoDrivers = function (drivers){
+    return drivers.slice(0,2);
+}
+
+// Code your solution in this file!
+const returnLastTwoDrivers = function (drivers){
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+const createFareMultiplier = function (fare){
+    return function(value){
+        return fare * value;
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers,func){
+    return func(drivers);
+}
